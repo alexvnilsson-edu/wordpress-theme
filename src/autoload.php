4ffdfee0906d051cc89f2c935ab6eb15b99a8122
alexvnilsson <alexvnilsson@outlook.com>
2020-10-02 17:43:33 +0200

    Refactor autoload.
    Thank you, mr. Dunham!

--- src/autoload.php
+++ src/autoload.php
@@ -1,30 +1,98 @@
 <?php
 namespace AlexVNilsson\WordPressTheme;
 
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
 define('DIRECTORY_SEPARATOR_BACKWARDS', '\\');
 
-Autoload::initialize();
-
+/**
+ * Simple Recursive Autoloader
+ *
+ * A simple autoloader that loads class files recursively starting in the directory
+ * where this class resides.  Additional options can be provided to control the naming
+ * convention of the class files.
+ *
+ * @package Autoload
+ * @license http://opensource.org/licenses/MIT  MIT License
+ * @author  Rob Dunham <contact@robunham.info>
+ */
 class Autoload
 {
+    /**
+     * File extension as a string. Defaults to ".php".
+     */
+    protected static $fileExt = '.php';
+
+    /**
+     * The top level directory where recursion will begin. Defaults to the current
+     * directory.
+     */
+    protected static $pathTop = __DIR__;
+
+    /**
+     * A placeholder to hold the file iterator so that directory traversal is only
+     * performed once.
+     */
+    protected static $fileIterator = null;
+
+    /**
+     * Initializer
+     */
     public static function initialize()
     {
-        spl_autoload_register(array(__CLASS__, 'register'));
+        spl_autoload_register(array(__CLASS__, "loader"));
     }
 
-    public static function register($className)
+    /**
+     * Autoload function for registration with spl_autoload_register
+     *
+     * Looks recursively through project directory and loads class files based on
+     * filename match.
+     *
+     * @param string $className
+     */
+    public static function loader($className)
     {
-        $extensions = array(".php", ".class.php", ".inc");
-        $paths = explode(PATH_SEPARATOR, "includes");
-        $className = str_replace("_", DIRECTORY_SEPARATOR_BACKWARDS, $className);
-        foreach ($paths as $path) {
-            $filename = $path . DIRECTORY_SEPARATOR_BACKWARDS . $className;
-            foreach ($extensions as $ext) {
-                if (is_readable($filename . $ext)) {
-                    require_once $filename . $ext;
-                    break;
+        $directory = new RecursiveDirectoryIterator(static::$pathTop, RecursiveDirectoryIterator::SKIP_DOTS);
+
+        if (is_null(static::$fileIterator)) {
+            static::$fileIterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::LEAVES_ONLY);
+        }
+
+        $filename = $className . static::$fileExt;
+
+        foreach (static::$fileIterator as $file) {
+            if (strtolower($file->getFilename()) === strtolower($filename)) {
+                if ($file->isReadable()) {
+                    include_once $file->getPathname();
                 }
+                break;
             }
         }
     }
+
+    /**
+     * Sets the $fileExt property
+     *
+     * @param string $fileExt The file extension used for class files.  Default is "php".
+     */
+    public static function setFileExt($fileExt)
+    {
+        static::$fileExt = $fileExt;
+    }
+
+    /**
+     * Sets the $path property
+     *
+     * @param string $path The path representing the top level where recursion should
+     *                     begin. Defaults to the current directory.
+     */
+    public static function setPath($path)
+    {
+        static::$pathTop = $path;
+    }
 }
+
+Autoload::setFileExt('.php');
+Autoload::initialize();
