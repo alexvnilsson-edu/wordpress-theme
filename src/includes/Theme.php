55408bb5937e6cb85cdf530f66fcdcace548d62b
alexvnilsson <alexvnilsson@outlook.com>
2020-10-02 17:44:42 +0200

    Refactor Theme.

--- src/includes/Theme.php
+++ src/includes/Theme.php
@@ -7,18 +7,9 @@
  */
 namespace AlexVNilsson\WordPressTheme;
 
-use AlexVNilsson\WordPressTheme\Core\Log;
-
 class Theme
 {
-    public function __construct()
+    public static function initialize()
     {
-    }
-
-    public static function foobar()
-    {
-        Log::getLogger()->notice("foobar says hello!");
     }
 }
-
-$theme = new Theme();
