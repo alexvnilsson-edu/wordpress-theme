a3c3cf485eda5ac6fad744fd7d04a4f901eed89b
alexvnilsson <alexvnilsson@outlook.com>
2020-10-02 04:06:29 +0200

    Change some stylesheets.

--- src/inc/enqueue.php
+++ src/inc/enqueue.php
@@ -8,13 +8,19 @@
 
 function alexvnilsson_load_scripts()
 {
+    // Google Font enqueues
     wp_enqueue_style('googlefont-robotoslab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;600;700&display=swap', false);
-    wp_enqueue_style('alexvnilsson-style', get_template_directory_uri() . '/style.min.css', array(), '{{ template.version }}', 'all');
-    // wp_deregister_script('jquery');
+    wp_enqueue_style('googlefont-materialicon', 'https://fonts.googleapis.com/icon?family=Material+Icons', false);
 
-    wp_enqueue_script('animejs', 'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.0/anime.min.js', false);
+
+    // Deregistration of unneeded scripts.
+    wp_deregister_script('jquery');
+
+    // Theme-specific script-enqueues
+    wp_enqueue_style('alexvnilsson-style', get_template_directory_uri() . '/style.min.css', array(), '{{ template.version }}', 'all');
+
+    // Theme-specific style-enqueues
     wp_enqueue_script('alexvnilsson-vendor', get_template_directory_uri() . "/assets/js/vendor.min.js", array(), '{{ template.version }}', true);
     wp_enqueue_script('alexvnilsson-main', get_template_directory_uri() . "/assets/js/main.min.js", array(), '{{ template.version }}', true);
-    // wp_enqueue_script('alexvnilsson');
 }
 add_action('wp_enqueue_scripts', 'alexvnilsson_load_scripts');
