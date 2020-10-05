464f490bd6fcdfa6abb757f371a09feef9d59950
alexvnilsson <alexvnilsson@outlook.com>
2020-10-05 17:59:29 +0200

    Update Enqueue.php.

--- src/includes/Enqueue.php
+++ src/includes/Enqueue.php
@@ -15,16 +15,20 @@ class Enqueue
     {
         add_action('wp_enqueue_scripts', array(__CLASS__, 'load_assets'));
         add_action('init', array(__CLASS__, 'register_blocks'));
+        add_action('admin_init', array(__CLASS__, 'load_admin_assets'));
     }
 
     public static function register_blocks()
     {
-        wp_enqueue_style('alexvnilsson-editor-style', get_template_directory_uri() . '/assets/css/editor/style.css', array(), '{{ template.version }}', 'all');
-
         wp_register_script('alexvnilsson-block-contact', get_template_directory_uri() . '/assets/js/blocks/contact.js', array('wp-blocks', 'wp-element'), '{{ template.version }}');
         register_block_type('alexvnilsson/contact', array(
             'editor_script' => 'alexvnilsson-block-contact'
         ));
+    }
+
+    public static function load_admin_assets()
+    {
+        wp_enqueue_style('alexvnilsson-editor-style', get_template_directory_uri() . '/assets/css/editor/style.css', array(), '{{ template.version }}', 'all');
     }
 
     public static function load_assets()
